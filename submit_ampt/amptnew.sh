396fd070810323323a0b4e7af70602550a51ad9e
TakeLeastAction <sunkaijiaxn@gmail.com>
2019-09-16 16:41:01 -0500

    initial code 02

--- submit_ampt/amptnew.sh
+++ submit_ampt/amptnew.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source /home/comp/root53424/bin/thisroot.sh
-export LD_LIBRARY_PATH=/usr/lib:/usr/lib64:/lib:/lib64:/usr/local/lib:/usr/local/lib64:$LD_LIBRARY_PATH
 
 Index=$(($1+1))
 
@@ -38,80 +36,8 @@ then
 fi
 mkdir $fileout
 cd ..
-cp -r ampt/* $fileout1
+cp -r ampt/code/* $fileout1
 cd $fileout1
-
-EFRM=2.5
-FRAME=CMS
-PROJ=A
-TARG=A
-IAP=197
-IZP=79
-IAT=197
-IZT=79
-NEVENT=100
-BMIN=0
-BMAX=3
-ISOFT=4
-NTMAX=150
-DT=0.2
-PARJ1=0.55
-PARJ2=0.15
-POPCORN=1
-PARJ5=1.0
-SHF=1
-QUF=0
-QUPA=2.0
-P0=2.0
-PASCMA=2.265d0
-IZPC=0
-ALPHA=0.33d0
-DPCOAL=1d6
-DRCOAL=1d6
-IHJSED=11
-RANSEED=13150909
-RANSEEP=8
-FLAGK=0
-FLAGPHI=1
-FLAGPI0=0
-OSCAR=0
-FLAGDEU=0
-INTEDEU=1
-CROSEC=1
-PT=-7
-MAXMISS=1000
-FLAGRADIA=3
-FLAGKT=1
-FLAGQUA=0
-PX=7.
-PY=0.
-X=0.
-Y=0.
-NSEMBD=1
-PSEMBD=5.
-TMAXEMBD=0.
-FLAGMOD=0
-FACTOR=1.d0
-FLAGORIEN=0
-#define parameters
-
-#infile=input.ampt
-#if [ -f ${infile} ]
-#then
-#    rm -f ${infile}
-#fi
-
-#echo -e $EFRM'\n'$FRAME'\n'$PROJ'\n'$TARG'\n'$IAP              >>${infile}
-#echo -e $IZP'\n'$IAT'\n'$IZT'\n'$NEVENT'\n'$BMIN               >>${infile}
-#echo -e $BMAX'\n'$ISOFT'\n'$NTMAX'\n'$DT'\n'$PARJ1             >>${infile}
-#echo -e $PARJ2'\n'$POPCORN'\n'$PARJ5'\n'$SHF'\n'$QUF           >>${infile}
-#echo -e $QUPA'\n'$P0'\n'$PASCMA'\n'$IZPC'\n'$ALPHA             >>${infile}
-#echo -e $DPCOAL'\n'$DRCOAL'\n'$IHJSED'\n'$RANSEED'\n'$RANSEEP  >>${infile}
-#echo -e $FLAGK'\n'$FLAGPHI'\n'$FLAGPI0'\n'$OSCAR'\n'$FLAGDEU   >>${infile}
-#echo -e $INTEDEU'\n'$CROSEC'\n'$PT'\n'$MAXMISS'\n'$FLAGRADIA   >>${infile}
-#echo -e $FLAGKT'\n'$FLAGQUA'\n'$PX,$PY'\n'$X,$Y                >>${infile}
-#echo -e $NSEMBD,$PSEMBD,$TMAXEMBD'\n'$FLAGMOD'\n'$FACTOR'\n'$FLAGORIEN'\n'   >>${infile}
-#parameters input infile
 
 
 system=`uname -s`
---
+++ submit_ampt/runampt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ..
+rm -rf data0
+mkdir data0
+cp -r ampt/input data0
+
+cd submit_ampt
+condor_submit ampt.con 
+
+
